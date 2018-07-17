3fb4bfa235d04675f240f2e70cef3b56bdbd3321
Pratibhasagar V <pratibav@thoughtworks.com>
2018-07-17 18:09:22 +0530

    Pratibha | Create the skeleton SpringBoot application

---
+++ src/main/java/com/thoughtworks/bahmnidhis2integrationservice/BahmniDhis2IntegrationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.bahmnidhis2integrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BahmniDhis2IntegrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BahmniDhis2IntegrationServiceApplication.class, args);
+	}
+}
