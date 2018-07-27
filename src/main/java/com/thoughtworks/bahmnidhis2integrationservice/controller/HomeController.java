ea2101123eff3b0c9eca54a5071a35d367730748
Pratibhasagar V <pratibav@thoughtworks.com>
2018-07-27 15:54:39 +0530

    Pratibha | #36 | Move HomeController to controller folder

--- src/main/java/com/thoughtworks/bahmnidhis2integrationservice/controller/HomeController.java
+++ src/main/java/com/thoughtworks/bahmnidhis2integrationservice/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.thoughtworks.bahmnidhis2integrationservice;
+package com.thoughtworks.bahmnidhis2integrationservice.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
