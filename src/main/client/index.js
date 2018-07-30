6127d53eac5b377f3f8510935a785dedc0440d54
Pratibhasagar V <pratibav@thoughtworks.com>
2018-07-30 11:05:46 +0530

    Pratibha | #36 | Resolve merge conflicts

--- src/main/client/index.js
+++ src/main/client/index.js
@@ -3,10 +3,11 @@ import {render} from 'react-dom';
 import TablesList from './components/TablesList.js';
 
 class App extends React.Component {
-  render() {
-      return (
-         <TablesList />
-      );
+    render() {
+        return (
+            <TablesList />
+        );
     }
 }
 
+render(<App/>, document.getElementById('app'));
