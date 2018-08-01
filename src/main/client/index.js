8240109cc7ccaa027ed28490a026538cbcf0c23f
Pratibhasagar V <pratibav@thoughtworks.com>
2018-08-01 14:08:11 +0530

    Pratibha | #36 | Add route for '/' path. Fix lint errors.

--- src/main/client/index.js
+++ src/main/client/index.js
@@ -1,57 +1,58 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+import { render } from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import reduxThunk from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
 import MappingDashboard from './mapping/mappingDashboard';
 import SyncDashboard from './sync/syncDashboard';
 import LogDashboard from './log/logDashboard';
-import Spinner from '../client/common/Spinner';
-import reduxThunk from "redux-thunk";
-import {applyMiddleware, createStore} from "redux";
-import { Provider } from 'react-redux';
-import reducers from "./Reducers";
+import Spinner from './common/Spinner';
+import reducers from './Reducers';
 
 const store = createStore(reducers, applyMiddleware(reduxThunk));
 
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = { loading: true };
-        this.setState = this.setState.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = { loading: true };
+    this.setState = this.setState.bind(this);
+  }
 
-    componentDidMount() {
-        this.setState({ loading: false });
-    }
+  componentDidMount() {
+    this.setState({ loading: false });
+  }
 
-    render() {
-        return (
-            <Provider store={store}>
-                <div className="form-builder-link">
-                    <Spinner show={this.state.loading} />
-                    <Link to={'/mapping'}>
-                        <i className="fa fa-map-signs"></i>
-                        Manage Mapping
-                    </Link>
-                    <Link to={'/sync'}>
-                        <i className="fa fa-upload"></i>
-                        Sync to DHIS
-                    </Link>
-                    <Link to={'/logs'}>
-                        <i className="fa fa-book"></i>
-                        Logs
-                    </Link>
-                    <Switch>
-                        <Route exact path='/mapping' component={MappingDashboard}/>
-                        <Route exact path='/sync' component={SyncDashboard}/>
-                        <Route exact path='/logs' component={LogDashboard}/>
-                    </Switch>
-                </div>
-            </Provider>
-        );
-    }
+  render() {
+    return (
+      <div className="form-builder-link">
+        <Spinner show={this.state.loading} />
+        <Link to="/mapping">
+          <i className="fa fa-map-signs" />
+            Manage Mapping
+        </Link>
+        <Link to="/sync">
+          <i className="fa fa-upload" />
+            Sync to DHIS
+        </Link>
+        <Link to="/logs">
+          <i className="fa fa-book" />
+            Logs
+        </Link>
+      </div>
+    );
+  }
 }
 
-render((<BrowserRouter>
-        <App/>
+render((
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route exact path="/mapping" component={MappingDashboard} />
+        <Route exact path="/sync" component={SyncDashboard} />
+        <Route exact path="/logs" component={LogDashboard} />
+      </div>
     </BrowserRouter>
+  </Provider>
 ), document.getElementById('app'));
