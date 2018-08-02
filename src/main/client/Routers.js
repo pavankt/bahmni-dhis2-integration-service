1674d31a6c4b7c88c98d8d8d118d4ed1e4fcf065
Pratibhasagar V <pratibav@thoughtworks.com>
2018-08-03 00:12:20 +0530

    Pratibha, Mahitha | #36 | Add BrowserHistory support. Update HomeButton functionality in Header.

--- src/main/client/Routers.js
+++ src/main/client/Routers.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import Header from "./common/Header";
 import LogDashboard from "./log/LogDashboard";
 import MappingDashboard from "./mapping/MappingDashboard";
 import SyncDashboard from "./sync/SyncDashboard";
 import App from './App';
 
+const browserHistory = createBrowserHistory();
+
 export function routes() {
-return (
-  <BrowserRouter>
-    <div>
-      <Route component={Header} />
-      <Route exact path="/" component={App} />
-      <Route exact path="/mapping" component={MappingDashboard} />
-      <Route exact path="/sync" component={SyncDashboard} />
-      <Route exact path="/logs" component={LogDashboard} />
-    </div>
-  </BrowserRouter>);
+    return (
+      <BrowserRouter history={browserHistory}>
+        <switch>
+          <Route component={Header} />
+          <Route exact path="/" component={App} />
+          <Route path="/mapping" component={MappingDashboard} />
+          <Route path="/sync" component={SyncDashboard} />
+          <Route path="/logs" component={LogDashboard} />
+        </switch>
+      </BrowserRouter>);
 }
