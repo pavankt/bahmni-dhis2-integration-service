4c1d0812e454996b4b329dca80a015386041e5e1
sudhamshk <sudhamsh543@gmail.com>
2018-08-13 00:29:28 +0530

    Sudhamsh | refactoring cmd support

--- scripts/builds/rpm/bahmni-dhis2-integration-service.sh
+++ scripts/builds/rpm/bahmni-dhis2-integration-service.sh
@@ -9,8 +9,14 @@ display_help(){
     RUN 'bahmni-dhis2-integration-service --help' for more information on a command \n"
 }
 
-launch_bahmni_mart(){
+launch_bahmni_dhis2_integration_service(){
     echo "Launching bahmni-dhis2-integration-service..."
     nohup java -jar /opt/bahmni-dhis2-integration-service/lib/bahmni-dhis2-integration-service-0.0.1-SNAPSHOT.jar --spring.config.location="/opt/bahmni-mart/properties/" >> /var/log/bahmni-dhis2-integration-service/bahmni-dhis2-integration-service.log 2>&1 &
     echo "Done"
 }
+
+if [[ -z $1 ]]; then
+    launch_bahmni_dhis2_integration_service;
+else
+    display_help
+fi
