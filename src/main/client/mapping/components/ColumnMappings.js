37f5c89f4e5db0d6ee7e8f24739b2123527abece
pavankt <pavankt@thoughtworks.com>
2018-08-27 16:24:41 +0530

    Pavan | #48 | Fixed bug of 'mapping persistance issue'- AC4 in #48

--- src/main/client/mapping/components/ColumnMappings.js
+++ src/main/client/mapping/components/ColumnMappings.js
@@ -1,79 +1,78 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import _ from "underscore";
 
 export default class ColumnMappings extends Component {
-  constructor() {
-    super();
-    this.renderColumns = this.renderColumns.bind(this);
-    this.insertValues = this.insertValues.bind(this);
-  }
+    constructor() {
+        super();
+        this.renderColumns = this.renderColumns.bind(this);
+        this.insertValues = this.insertValues.bind(this);
+    }
 
-  componentDidMount() {
-      this.insertValues();
-  }
+    componentDidMount() {
+        this.insertValues();
+    }
 
-  componentDidUpdate() {
-      this.insertValues();
-  }
+    componentDidUpdate() {
+        this.insertValues();
+    }
 
-  insertValues() {
-      let mappingJson = this.props.mappingJson;
+    shouldComponentUpdate(nextProps) {
+        return !(_.isEqual(this.props.columns, nextProps.columns));
+    }
 
-      if(!this.isEmptyObject(mappingJson)){
-          this.props.columns.forEach(column => {
-              let mapping = mappingJson[column];
-              this.refs[column].value = mapping !== undefined ? mapping : '';
-          });
-      }
-  }
+    insertValues() {
+        let mappingJson = this.props.mappingJson;
 
-    isEmptyObject(object) {
-        return object === undefined || Object.keys(object).length === 0;
+        this.props.columns.forEach(column => {
+            let mapping = mappingJson[column];
+            this.refs[column].value = mapping !== undefined ? mapping : '';
+        });
     }
 
     renderColumns() {
-    return this.props.columns.map(column => (
-      <tr key={column} className={`mapping-row table-row ${this.props.category}`}>
-        <td className="mapping-column-name">
-          {column}
-        </td>
-        <td className="mapping-data-element">
-          <input type="text" className="mapping-input" ref={column} />
-        </td>
-      </tr>
-    ));
-  }
-
-  render() {
-    return (
-      <div className="mapping-table-div">
-        {/*eslint-disable*/}
-        <span className="enrollment-table-span">
-            Please provide DHIS2 data element mapping for {this.props.mappingType}
-        </span>
-        {/*eslint-enable*/}
-        <section className="column-mapping-section">
-          <table className="mapping-table">
-            <tr className="mapping-row-header">
-              <th className="mapping-header">
-                    Bahmni Data Point
-              </th>
-              <th className="mapping-header">
-                    DHIS2 Data Element ID
-              </th>
+        return this.props.columns.map(column => (
+            <tr key={column} className={`mapping-row table-row ${this.props.category}`}>
+                <td className="mapping-column-name">
+                    {column}
+                </td>
+                <td className="mapping-data-element">
+                    <input type="text" className="mapping-input" ref={column}/>
+                </td>
             </tr>
-            {this.renderColumns()}
-          </table>
-        </section>
-      </div>
-    );
-  }
+        ));
+    }
+
+    render() {
+        return (
+            <div className="mapping-table-div">
+                {/*eslint-disable*/}
+                <span className="enrollment-table-span">
+                    Please provide DHIS2 data element mapping for {this.props.mappingType}
+                </span>
+                {/*eslint-enable*/}
+                <section className="column-mapping-section">
+                    <table className="mapping-table">
+                        <tr className="mapping-row-header">
+                            <th className="mapping-header">
+                                Bahmni Data Point
+                            </th>
+                            <th className="mapping-header">
+                                DHIS2 Data Element ID
+                            </th>
+                        </tr>
+                        {this.renderColumns()}
+                    </table>
+                </section>
+            </div>
+        );
+    }
 }
 
 ColumnMappings.propTypes = {
-  columns: PropTypes.array.isRequired,
-  mappingJson: PropTypes.object.isRequired,
-  category: PropTypes.string.isRequired,
-  mappingType: PropTypes.string.isRequired
+    columns: PropTypes.array.isRequired,
+    mappingJson: PropTypes.object.isRequired,
+    category: PropTypes.string.isRequired,
+    mappingType: PropTypes.string.isRequired
 
 };
