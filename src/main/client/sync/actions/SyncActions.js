c5f4c0a9a3fb4e56c7652ca6aeb436df9ceec818
Pratibhasagar V <pratibav@thoughtworks.com>
2018-09-29 11:36:51 +0530

    Pratibha | ### | Move the params to request body for the PUT request.

--- src/main/client/sync/actions/SyncActions.js
+++ src/main/client/sync/actions/SyncActions.js
@@ -7,9 +7,13 @@ export function syncData(mappingName = '', user = '') {
     return async dispatch => {
         try {
             let ajax = Ajax.instance();
+            let body = {
+                service: mappingName,
+                user: user
+            }
             dispatch(showMessage("Sync started for " + mappingName, "success"));
             auditLog(auditLogEventDetails.SEND_DATA_TO_DHIS);
-            await ajax.put(sync.URI + '?service=' + mappingName + '&user=' + user);
+            await ajax.put(sync.URI, body);
         } catch (e) {
             dispatch(showMessage("No data to sync for " + mappingName, "error"));
         }
