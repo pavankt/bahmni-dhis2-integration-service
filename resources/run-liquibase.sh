9b2bc93b0ba3fab15b174755c84737ce87a25b01
sudhamshk <sudhamsh543@gmail.com>
2018-10-04 16:51:02 +0530

    Sudhamsh | #000 | adding liquibase_dhis_sync_role to run during installation

--- resources/run-liquibase.sh
+++ resources/run-liquibase.sh
@@ -7,3 +7,4 @@ fi
 
 
 liquibase --driver='org.postgresql.Driver' --classpath=$postgresql_jar_location/$postgresql_jar_name --changeLogFile=$liquibase_changelog_path --defaultSchemaName='public' --url=jdbc:postgresql://$postgres_db_server:$analytics_db_port/$analytics_db_name --username=$analytics_db_user --password=$analytics_db_password update >> /bahmni_temp/logs/bahmni_deploy.log 2>> /bahmni_temp/logs/bahmni_deploy.log
+liquibase --driver='org.postgresql.Driver' --classpath=$postgresql_jar_location/$postgresql_jar_name --changeLogFile=$liquibase_custom_changelog_path --defaultSchemaName='public' --url=jdbc:postgresql://$postgres_db_server:$analytics_db_port/$analytics_db_name --username=$analytics_db_user --password=$analytics_db_password update >> /bahmni_temp/logs/bahmni_deploy.log 2>> /bahmni_temp/logs/bahmni_deploy.log
