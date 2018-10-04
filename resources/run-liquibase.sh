2710c5ade3eaec3a223d2d84420e6e7bba2f1291
sudhamshk <sudhamsh543@gmail.com>
2018-10-04 17:59:43 +0530

    Sudhamsh | #000 | adding liquibase_dhis_sync_role to run during installation

--- resources/run-liquibase.sh
+++ resources/run-liquibase.sh
@@ -5,6 +5,10 @@ if [ -f /etc/bahmni-dhis2-integration-service/bahmni-dhis.conf ]; then
 . /etc/bahmni-dhis2-integration-service/bahmni-dhis.conf
 fi
 
+CHANGE_LOG_TABLE="-Dliquibase.databaseChangeLogTableName=liquibasechangelog -Dliquibase.databaseChangeLogLockTableName=liquibasechangeloglock"
+LIQUIBASE_JAR="/opt/openmrs/openmrs/WEB-INF/lib/liquibase-core-2.0.5.jar"
+DRIVER="com.mysql.jdbc.Driver"
+COMMON_CLASSPATH="/opt/openmrs/openmrs.war"
 
 liquibase --driver='org.postgresql.Driver' --classpath=$postgresql_jar_location/$postgresql_jar_name --changeLogFile=$liquibase_changelog_path --defaultSchemaName='public' --url=jdbc:postgresql://$postgres_db_server:$analytics_db_port/$analytics_db_name --username=$analytics_db_user --password=$analytics_db_password update >> /bahmni_temp/logs/bahmni_deploy.log 2>> /bahmni_temp/logs/bahmni_deploy.log
-liquibase --driver='org.postgresql.Driver' --classpath=$postgresql_jar_location/$postgresql_jar_name --changeLogFile=$liquibase_custom_changelog_path --defaultSchemaName='public' --url=jdbc:postgresql://$postgres_db_server:$analytics_db_port/$analytics_db_name --username=$analytics_db_user --password=$analytics_db_password update >> /bahmni_temp/logs/bahmni_deploy.log 2>> /bahmni_temp/logs/bahmni_deploy.log
+java $CHANGE_LOG_TABLE -jar $LIQUIBASE_JAR --driver=$DRIVER --classpath=$COMMON_CLASSPATH --changeLogFile=$liquibase_custom_changelog_path --url=jdbc:mysql://$OPENMRS_DB_SERVER:3306/openmrs --username=$OPENMRS_DB_USERNAME --password=$OPENMRS_DB_PASSWORD update >> /bahmni_temp/logs/bahmni_deploy.log 2>> /bahmni_temp/logs/bahmni_deploy.log
