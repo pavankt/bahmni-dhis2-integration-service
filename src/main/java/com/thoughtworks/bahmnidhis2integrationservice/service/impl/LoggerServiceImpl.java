35a409653d2abc223291874d592894701c36cb08
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-10-06 14:10:22 +0530

    Mahitha | #70 | 1. Get logs depends on the given parameters 2. Moved LogDashboard to components from logs

---
+++ src/main/java/com/thoughtworks/bahmnidhis2integrationservice/service/impl/LoggerServiceImpl.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.bahmnidhis2integrationservice.service.impl;
+
+import com.thoughtworks.bahmnidhis2integrationservice.dao.impl.LoggerDAOImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class LoggerServiceImpl {
+
+    @Autowired
+    private LoggerDAOImpl loggerDAO;
+
+    public List<Map<String, Object>> getLogs(String date, String user, String service, boolean getAbove, int logId) {
+        return loggerDAO.getLogs(date, user, service, getAbove, logId);
+    }
+}
